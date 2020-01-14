ab4fb2523491e84e16d080d5d93b9611b17e5576
Shalini Maiti <shalini.maiti@student.tugraz.at>
2020-01-15 00:06:25 +0100

    Integrated, added classification and the rest of the framework. WIP: Testing and finetuning of the model.

--- gesture_recognizer.py
+++ gesture_recognizer.py
@@ -6,11 +6,14 @@ Created on Fri Nov 15 21:26:27 2019
 @author: chetansrinivasakumar
 """
 
-import numpy as np 
+import numpy as np
+import glob
+import os
 import matplotlib.pyplot as plt
 import cv2 as cv
-import scipy 
+import scipy
 from jeanCV import skinDetector
+
 
 def argmax2d(X):
     ind = np.unravel_index(np.argmax(np.uint32(X), axis=None), X.shape)
@@ -29,13 +32,13 @@ def findMassCenter(im):
     return int(mean[0]), int(mean[1])
 
 def findHandDistanceSignature(palmPoint, mcPoint, contours):
-    #Collect contour points. 
+    #Collect contour points.
     border_points = []
     for i in range(len(contours)):
         for j in range(contours[i].shape[0]):
             pt = contours[i][j, :, :]
             border_points.append(pt)
-    
+
     print("border points shape: ", len(border_points))
     distanceList = []
     thetaList = []
@@ -43,9 +46,9 @@ def findHandDistanceSignature(palmPoint, mcPoint, contours):
     palmY = palmPoint[1]
     mcX = mcPoint[0]
     mcY = mcPoint[1]
-    baselineVec = (palmX - mcX, palmY - mcY)    
+    baselineVec = (palmX - mcX, palmY - mcY)
     baselineDist = np.linalg.norm(baselineVec)
-    
+
     angles = []
     distances = []
     for i in range(len(border_points)):
@@ -60,43 +63,86 @@ def findHandDistanceSignature(palmPoint, mcPoint, contours):
             ang = ang + 360
         distances.append(borderPointDist)
         angles.append(ang)
-    
-    
+
+
     n_bins = 40
     bins = np.linspace(0, 360, n_bins)
     hist, bin_edges = np.histogram(a=angles, bins=bins, range=None, normed=None, weights=distances, density=True)
-    
+
     print(hist)
-            
-        
+
+
     return hist
 
-    
-if __name__ == "__main__":
-    hand = cv.imread("hand.jpg")
-    detector = skinDetector("hand.jpg")
+def generate_descriptor(gesture_image):
+    print("gesture_image", gesture_image)
+    hand = cv.imread(gesture_image) # Replace hand.jpg with gesture_image
+    #print("hand", hand)
+
+    detector = skinDetector(gesture_image)
     detector.find_skin()
 
     palmX, palmY = findPalmPoint(detector.binary_mask_image)
     print("Palm X: ", palmX, " Palm Y: ", palmY)
-    
+
     mcX, mcY = findMassCenter(detector.binary_mask_image)
     print("Mc X: ", mcX, " Mc Y: ", mcY)
-    
+
     thresh_mask = detector.binary_mask_image.astype(np.uint8)
     im, contours, hierarchy = cv.findContours(detector.binary_mask_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     cv.drawContours(hand, contours, -1, (255, 0, 0), 3)
-    
+
     distSig = findHandDistanceSignature((palmX, palmY), (mcX, mcY), contours)
     print("Contour Shape: ", len(contours))
+    return distSig
 
-    f = plt.figure()
-    f.add_subplot(1, 2, 1)
-    handPlt = cv.drawMarker(hand, (palmX, palmY), (255, 0, 0), markerType=cv.MARKER_CROSS, markerSize=15, thickness=2, line_type=cv.LINE_AA)
-    handPlt = cv.drawMarker(handPlt, (mcX, mcY), (0, 255, 0), markerType=cv.MARKER_CROSS, markerSize=15, thickness=2, line_type=cv.LINE_AA)
-    plt.imshow(handPlt)
-    f.add_subplot(1, 2, 2)
-    plt.imshow(detector.binary_mask_image)
-    
+def generate_descriptor_stack(image_folder):
+  print(image_folder)
+  gesture_types = ["A", "B", "C", "Five", "Point", "V"]
+  stack_of_descriptors = []
+  labels_array = []
+  for gesture_type in gesture_types:
+    gesture_path = image_folder + gesture_type
+    print("gesture_path", gesture_path)
+    for gesture_image in glob.glob(gesture_path + '/uniform/*.ppm'):
+      hand = cv.imread(gesture_image) # Replace hand.jpg with gesture_image
+      #print("hand", hand)
 
-    
+      detector = skinDetector(gesture_image)
+      detector.find_skin()
+
+      palmX, palmY = findPalmPoint(detector.binary_mask_image)
+      print("Palm X: ", palmX, " Palm Y: ", palmY)
+
+      mcX, mcY = findMassCenter(detector.binary_mask_image)
+      print("Mc X: ", mcX, " Mc Y: ", mcY)
+
+      thresh_mask = detector.binary_mask_image.astype(np.uint8)
+      im, contours, hierarchy = cv.findContours(detector.binary_mask_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+      cv.drawContours(hand, contours, -1, (255, 0, 0), 3)
+
+      distSig = findHandDistanceSignature((palmX, palmY), (mcX, mcY), contours)
+      print("Contour Shape: ", len(contours))
+
+      f = plt.figure()
+      f.add_subplot(1, 2, 1)
+      handPlt = cv.drawMarker(hand, (palmX, palmY), (255, 0, 0), markerType=cv.MARKER_CROSS, markerSize=15, thickness=2, line_type=cv.LINE_AA)
+      handPlt = cv.drawMarker(handPlt, (mcX, mcY), (0, 255, 0), markerType=cv.MARKER_CROSS, markerSize=15, thickness=2, line_type=cv.LINE_AA)
+      plt.imshow(handPlt)
+      f.add_subplot(1, 2, 2)
+      plt.imshow(detector.binary_mask_image)
+      stack_of_descriptors.append(distSig)
+      labels_array.append(gesture_type)
+      print("DEscriptor sHape", np.array(stack_of_descriptors).shape)
+  return stack_of_descriptors, labels_array
+
+if __name__ == "__main__":
+  image_folder_path = "/Users/shalini/Documents/TU_Graz/Third_Semester/ImageUnderstandingPracticals/simple_hand_gesture_recognition/Marcel-Test/"
+  descriptor_training_data, training_labels = generate_descriptor_stack(image_folder_path)
+  np.save("descriptor_training_data", descriptor_training_data, allow_pickle=True)
+  np.save("descriptor_training_labels", training_labels, allow_pickle=True)
+  #old_training_data = np.load("descriptor_training_data.npy")
+  #old_labels_data = np.load("descriptor_training_labels.npy")
+  #print(old_training_data)
+  #print(old_labels_data)
+
--- simple_recognition.py
+++ simple_recognition.py
@@ -16,8 +16,15 @@ import cv2
 import imutils
 import numpy as np
 from sklearn.metrics import pairwise
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
 import os
 from sklearn.svm import SVC
+import random
+from gesture_recognizer import generate_descriptor
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import pickle
 
 #-----------------
 # TRAINING
@@ -26,42 +33,38 @@ from sklearn.svm import SVC
 # global variables
 bg = None
 cwd = os.getcwd()
-descriptors = np.radom.normal(0.5, 1, size=(12, 12)) #Todo: Store real descriptors
+#descriptors = np.random.normal(0.5, 1, size=(12, 12)) #Todo: Store real descriptors
 labels = range(12)
 sample_images = [cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm", cwd + "/Marcel-Test/A/uniform/A-uniform29.ppm"]
 
-# Generate descriptors for training
-
-# Generate knn model
-knn_model = knn_classifcation(descriptors, labels)
-accuracies = []
-
-# Generate svm model
-svm_model = svm_clssification(descriptors, labels)
 
 def generate_features_and_labels(descriptors, labels):
     #creating labelEncoder
-    le = preprocessing.LabelEncoder()
+    le = preprocessing.OneHotEncoder()
+    print("descriptors SHAPE", descriptors.shape)
+    print("LABEL SHAPE", labels.shape)
     # Converting string labels into numbers.
-    features = le.fit_transform(descriptors)
+    features = le.fit_transform(descriptors, labels)
+    print(features[0])
     encoded_labels = le.fit_transform(labels)
-    
+    print("FEATURES SHAPE", features.shape)
+    print("LABEL SHAPE", encoded_labels.shape)
     return features, encoded_labels
 
-def knn_classifcation(descriptors, labels):
-    features, encoded_labels = generate_features_and_labels(descriptors, labels)
+def knn_classification(descriptors, labels):
+    #features, encoded_labels = generate_features_and_labels(descriptors, labels)
     model = KNeighborsClassifier(n_neighbors=3)
-    model.fit(features,encoded_labels)
+    model.fit(descriptors,labels)
     return model
 
-def svm_clssification(descriptors, labels):
-    features, encoded_labels = generate_features_and_labels(descriptors, labels)
-    clf = SVC(gamma='auto')
-    clf.fit(features, encoded_labels)
+def svm_classification(descriptors, labels):
+    #features, encoded_labels = generate_features_and_labels(descriptors, labels)
+    model = SVC(gamma='auto')
+    model.fit(descriptors, labels)
     return model
-    
+
 def prediction(model, test_descriptor):
-    predicted= model.predict(test_descriptor)
+    predicted= model.predict([test_descriptor])
     print(predicted)
     return predicted
 
@@ -71,12 +74,40 @@ def prediction(model, test_descriptor):
 
 def map_sample_images(label, sample_images):
     return sample_images[label]
-    
+
 
 #-----------------
 # MAIN FUNCTION
 #-----------------
 if __name__ == "__main__":
+
+    # Load descriptors and labels for training
+    descriptors = np.load('descriptor_training_data.npy')
+    descriptors = descriptors
+    print("descriptors_shape", descriptors.shape)
+    labels = np.load('descriptor_training_labels.npy')
+    print("labels_shape", labels.shape)
+
+    # Train and save data
+    if os.path.exists("knn_training_weights.sav"):
+      # Load knn model
+      knn_model = pickle.load(open("knn_training_weights.sav", 'rb'))
+    else:
+      # Generate knn model
+      knn_model = knn_classification(descriptors, labels)
+      pickle.dump(knn_model, open("knn_training_weights.sav", 'wb'))
+
+    accuracies_knn = []
+
+    if os.path.exists("svm_training_weights.sav"):
+      #Load svm model
+      svm_model = pickle.load(open("svm_training_weights.sav", 'rb'))
+    else:
+      # Generate svm model
+      svm_model = svm_classification(descriptors, labels)
+      pickle.dump(svm_model, open("svm_training_weights.sav", 'wb'))
+    accuracies_svm = []
+
     # initialize accumulated weight
     accumWeight = 0.5
 
@@ -88,13 +119,13 @@ if __name__ == "__main__":
 
     # initialize num of frames
     num_frames = 0
-    
+
     # random snapshots
-    snapshots = np.random.randint(0, 1000, size=20)
+    snapshots = np.random.randint(0, 100, size=20)
     # keep looping, until interrupted
     while(True):
         # get the current frame
-        (grabbed, frame) = camera.read()
+        grabbed, frame = camera.read()
 
         # resize the frame
         frame = imutils.resize(frame, width=700)
@@ -112,41 +143,48 @@ if __name__ == "__main__":
         roi = frame[top:bottom, right:left]
 
         # Todo: change this? convert the roi to grayscale and blur it
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        #gray = clone
+        #gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
         # Todo: Generate test descriptor
-        test_descriptor = random.random.normal(0, 1, size=(12, 1))
-
+        #plt.imshow(gray)
+        cv2.imwrite('image_temp.jpg', frame)
+        test_descriptor = generate_descriptor('image_temp.jpg')
+        print("test_descriptor", test_descriptor.shape)
         # Todo: Make predictions. Test.
         predicted_knn = prediction(knn_model, test_descriptor)
-        predicted_svm = np.random.randint(11)
-        
+        predicted_svm = prediction(svm_model, test_descriptor)
+
         # Todo: Visualize the result
         cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
-        cv2.putText(clone, "KNN:"predicted_knn + "SVM:" + predicted_svm, pos, cv2.FONT_HERSHEY_SIMPLEX, 0.4, (1, 0, 0), 1, cv2.LINE_AA)
-        
+#        cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
+        cv2.putText(clone, "KNN:" + str(predicted_knn) + "SVM:" + str(predicted_svm), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (1, 0, 0), 1, cv2.LINE_AA)
+
         # increment the number of frames
         num_frames += 1
 
         # display the frame with segmented hand
-        cv2.imshow("Video Feed", clone)
-        
+        cv2.imshow("Video Feed" + str(num_frames), clone)
+
+
         # save snapshots with their classification
         if num_frames in snapshots:
-            accuracies.append(num_frames, predicted)
-            cv.imsave(predicted + "_gesture" + num_frames, gray)
+            accuracies_knn.append([num_frames, predicted_knn])
+            cv2.imwrite('snapshots/'+ str(num_frames) + '.jpg', clone)
+            accuracies_svm.append([num_frames, predicted_svm])
+            #cv2.imwrite('snapshots/'+ predicted_svm + "_gesture" + num_frame + '.jpg', clone)
 
         # observe the keypress by the user
-        keypress = cv2.waitKey(1) & 0xFF
-
-        # if the user pressed "q", then stop looping
-        if keypress == ord("q"):
-            break
-
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+        print(num_frames)
 # free up memory
 camera.release()
 cv2.destroyAllWindows()
+np.save("accuracies_knn", accuracies_knn) # Manually annotate
+np.save("accuracies_svm", accuracies_svm) # Manually annotate
+print("Fin")
 
 #-----------------
 # VISUALIZATION
